da9ecdcf00933199e3896311075542f8f76874d6
bm-bosch-edmunds <bmolina.mobileapps.company@gmail.com>
2016-06-30 00:31:11 -0400

    Created string and dimension resources for many items in the layouts.

--- app/src/main/java/com/texocoyotl/ptedmundscars/data/CarsDatabaseHelper.java
+++ app/src/main/java/com/texocoyotl/ptedmundscars/data/CarsDatabaseHelper.java
@@ -6,7 +6,7 @@ import android.database.sqlite.SQLiteOpenHelper;
 
 public class CarsDatabaseHelper  extends SQLiteOpenHelper {
     private static final String DATABASE_NAME = "edmunds.db";
-    private static final int DATABASE_VERSION = 4;
+    private static final int DATABASE_VERSION = 5;
 
     public CarsDatabaseHelper(Context context) {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
